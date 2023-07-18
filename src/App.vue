16e58631572a4e7f064a324971f08676f149a24d
mahima98 <mahimaramgolam98@gmail.com>
2023-07-18 23:42:59 +0400

    Add page transition

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,18 @@ import Header from "./components/Header.vue";
 
 <template>
   <Header />
-  <router-view></router-view>
+  <Transition name="fade" mode="out-in">
+    <router-view></router-view>
+  </Transition>
 </template>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
